b6515478230bced58c1e0176803f1b3d8c2faef1
ujjwalp360 <ujjwalp360@gmail.com>
2024-11-07 03:09:37 +0000

    cvc

--- src/portfolio.js
+++ src/portfolio.js
@@ -11,7 +11,7 @@ const seo = {
   description:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   og: {
-    title: "Ashutosh Hathidara Portfolio",
+    title: "Ujjwal Barange",
     type: "website",
     url: "http://ashutoshhathidara.com/",
   },
